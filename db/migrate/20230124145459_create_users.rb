e103b5d177b565da590b22fb2c0765c3536c746a
KareemWilson <kareemmagdywilson@gmail.com>
2023-01-26 18:04:02 +0200

    Update Migration Files

--- db/migrate/20230124145459_create_users.rb
+++ db/migrate/20230124145459_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :Name
-      t.string :Photo
-      t.string :Bio
-      t.integer :PostsCounter
+      t.string :name
+      t.string :photo
+      t.string :bio
+      t.integer :posts_counter
 
       t.timestamps
     end
