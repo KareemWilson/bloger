6a3eecb9133d3f718a88386d8cf248a744b1cb34
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-01 12:27:24 +0200

    Adjust Migration files

--- db/migrate/20230124165641_add_counters_to_posts.rb
+++ db/migrate/20230124165641_add_counters_to_posts.rb
@@ -1,6 +1,6 @@
 class AddCountersToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_column :posts, :comments_counter, :integer
-    add_column :posts, :likes_counter, :integer
+    add_column :posts, :comments_counter, :integer, default: 0
+    add_column :posts, :likes_counter, :integer, default: 0
   end
 end
