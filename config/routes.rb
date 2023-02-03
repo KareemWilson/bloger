24d5efd0b9cab3dbf6eac4a5c91c7b70d77b194c
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-03 15:26:03 +0200

    Adjust routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index'
-  get '/users/:user_id', to: 'users#show'
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:post_id', to: 'posts#show'
+  # get '/users', to: 'users#index'
+  # get '/users/:user_id', to: 'users#show'
+  # get '/users/:user_id/posts', to: 'posts#index'
+  # get '/users/:user_id/posts/new', to: 'posts#new'
+  # post 'posts', to: 'posts#create'
+  # get '/users/:user_id/posts/:post_id', to: 'posts#show'
+
+  root 'users#index'
+  resources :users do
+    resources :posts do 
+      resources :comments
+      resources :likes
+    end
+  end
 end
