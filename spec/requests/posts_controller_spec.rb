50abc4e9a84164e59cb491723d7471469c6b6b4e
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-08 11:46:35 +0200

    Fix old tests rspec requests

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,21 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Posts Management', type: :request do
   it 'Render the correct view' do
-    get '/users/:id/posts'
+    get '/users/1/posts'
     expect(response).to be_successful
     expect(response).to render_template(:index)
-    expect(response.body).to include('Here is all posts for a given user')
 
-    get '/users/:user_id/posts/:post_id'
+    get '/users/1/posts/1'
     expect(response).to be_successful
     expect(response).to render_template(:show)
-    expect(response.body).to include('Here is a post for a given user')
   end
   it "Doesn't render the wrong view" do
-    get '/users/:id/posts'
+    get '/users/1/posts'
     expect(response).to_not render_template(:show)
 
-    get '/users/:user_id/posts/:post_id'
+    get '/users/1/posts/1'
     expect(response).to_not render_template(:index)
   end
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -5,18 +5,16 @@ RSpec.describe 'Users Management', type: :request do
     get '/users'
     expect(response).to be_successful
     expect(response).to render_template(:index)
-    expect(response.body).to include('Here is all users')
 
-    get '/users/:id'
+    get '/users/1'
     expect(response).to be_successful
     expect(response).to render_template(:show)
-    expect(response.body).to include('Here is a user by a given id')
   end
   it "Doesn't render the wrong view" do
     get '/users'
     expect(response).to_not render_template(:show)
 
-    get '/users/:id'
+    get '/users/1'
     expect(response).to_not render_template(:index)
   end
 end
