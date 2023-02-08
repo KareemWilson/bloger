366ffcb252853807e8aadc714dface3cd9c3266f
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-08 14:16:48 +0200

    Adjust request spec tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Management', type: :request do
+  before(:each) do 
+    @user_1 = User.create(name: 'Kareem', photo: 'https://i.imgur.com/9yG7zZT.jpg')
+    @post_1 = Post.create(title: 'Post number #2', text: 'This is post 2', user: @user_1)
+  end
   it 'Render the correct view' do
     get '/users'
     expect(response).to be_successful
