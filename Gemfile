f5bf6fb737b7f324864f6a34928e7b38471811be
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-09 21:39:11 +0200

    Fix linter Errors

--- Gemfile
+++ Gemfile
@@ -27,8 +27,8 @@ gem 'turbo-rails'
 gem 'stimulus-rails'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
-gem 'jbuilder'
 gem 'devise'
+gem 'jbuilder'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
