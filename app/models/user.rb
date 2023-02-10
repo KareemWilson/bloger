1e4eec4752df47e8af3611b71bfb6ca7cad166b3
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-10 19:22:02 +0200

    Add roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :email_confirmation
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :posts
@@ -12,8 +10,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, confirmation: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  Roles = [ :admin , :default ]
+
 
   def last_3_posts
     posts.order(created_at: :DESC).limit(3)
+  end
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
   end
 end
