6d8f2023ad4312c8f22f0cc8027e361a34ded032
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-10 16:09:14 +0200

    Handling cancan errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to root_url, alert: exception.message
+  end
+
   protected
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
+
 end
