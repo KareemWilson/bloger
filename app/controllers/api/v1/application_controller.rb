87fc015d11aff5e50c7e8e1086e572f357608be8
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-11 12:31:41 +0200

    Add api controllers

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::ApplicationController < ActionController::API
+    respond_to :json
+  end
+  
---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::CommentsController < Api::V1::ApplicationController
+    def index
+      comments = Comment.where(post_id: params[:post_id], user_id: params[:user_id])
+      render json: comments, status: :ok
+    end
+  
+    def create
+      post_id = params[:post_id]
+      text = params[:text]
+      request_body = JSON.parse(request.body.read)
+      user_id = request_body["user_id"]
+      comment = Comment.new(text:, post_id:, user_id:)
+  
+      if comment.save
+        render json: comment, status: :ok
+      else
+        render json: comment.errors, status: :unprocessable_entity
+      end
+    end
+  end
+  
